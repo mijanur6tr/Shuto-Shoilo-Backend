54e86a86bbca56f8669e4f7520a465baa111d478
Mijanur <mijanur6tr123@gmail.com>
2025-07-12 19:08:03 +0600

    console

--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -1,18 +1,22 @@
 import jwt from 'jsonwebtoken';
 
 export const loginAdmin = (req, res) => {
+  console.log("Login Attempt:", req.body);
+
   const { email, password } = req.body;
 
-  if (
-    email === process.env.ADMIN_EMAIL &&
-    password === process.env.ADMIN_PASSWORD
-  ) {
-    const token = jwt.sign({ role: "admin" }, process.env.JWT_SECRET_KEY, {
-      expiresIn: "2h",
-    });
+  console.log("Received:", email, password);
+  console.log("Expected:", process.env.ADMIN_EMAIL, process.env.ADMIN_PASSWORD);
 
-    return res.status(200).json({ success: true, token });
+  if (email !== process.env.ADMIN_EMAIL || password !== process.env.ADMIN_PASSWORD) {
+    console.log("Unauthorized access");
+    return res.status(401).json({ success: false, message: "Invalid credentials" });
   }
 
-  return res.status(401).json({ success: false, message: "Invalid credentials" });
+  
+  const token = jwt.sign({ role: "admin" }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "2h",
+  });
+
+  return res.status(200).json({ success: true, token });
 };
