6a7df588c6ba854e72c8eabd0fbcac3267ef642e
Mijanur <mijanur6tr123@gmail.com>
2025-07-12 20:41:53 +0600

    done

--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -1,12 +1,9 @@
 import jwt from 'jsonwebtoken';
 
 export const loginAdmin = (req, res) => {
-  console.log("Login Attempt:", req.body);
-
+ 
   const { email, password } = req.body;
 
-  console.log("Received:", email, password);
-  console.log("Expected:", process.env.ADMIN_EMAIL, process.env.ADMIN_PASSWORD);
 
   if (email !== process.env.ADMIN_EMAIL || password !== process.env.ADMIN_PASSWORD) {
     console.log("Unauthorized access");
